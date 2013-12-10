bbfb2e43804c90d944d54b2d4ea1c5e41fc89f60
Jan Varwig <jan@varwig.org>
2013-12-10 20:34:45 +0100

    init

---
+++ src/OT/Operations.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+module OT.Operations where 
+
+import Data.Functor
+import Control.Applicative
+import Data.Aeson
+import Data.Vector
+import Data.Text
+
+type Property = Text
+type Position = Int
+data PathSegment = PropSeg Property | PosSeg Position
+                 deriving Show
+type Path = [PathSegment]
+
+data JSONNumberOperation = Add Path Int
+                         deriving Show
+data JSONStringOperations = StringInsert Path Position Text
+                          | StringDelete Path Position Text
+                          deriving Show
+data JSONArrayOperation = ArrayInsert  Path Position Value
+                        | ArrayDelete  Path Position Value
+                        | ArrayReplace Path Position Value Value
+                        | ArrayMove    Path Position Int
+                        deriving Show
+data JSONObjectOperation = Insert  Path Property Value
+                         | Delete  Path Property 
+                         | Replace Path Property Value Value
+                         deriving Show
+
+instance FromJSON PathSegment where
+  parseJSON (String a) = PropSeg <$> parseJSON (String a)
+  parseJSON (Number a) = PosSeg  <$> parseJSON (Number a)
+  parseJSON _ = fail "Invalid Pathsegment"
+
+instance FromJSON Path where
+  parseJSON (Array a) = parseJSON <$> a
+  parseJSON _ = fail "Can't convert non-array to Path"
+
+{-instance FromJSON JSONNumberOperation where-}
+  {-parseJSON (Object v) = Add <$> (v .: "p") <*> (v .: "ni")-}
+  {-parseJSON _          = fail-}
+
+{-opMap (Add path a) = Data.Map.fromList [("p", path), ("na", a)]-}
+{-opMap (StringInsert path pos str) = Data.Map.fromList [("si", )]-}
+
+
+{-renderOperation (Add path num) = "{ p:"++(show path)++", na:"++(show num)++"}"-}
